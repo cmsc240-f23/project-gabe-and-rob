e5299918ba34e9c17c28a2a5959df1dec71942e9
Gabe Greenberg <gabe.greenberg@richmond.edu>
2023-11-28 11:40:26 -0500

    update

---
+++ Objects.cpp
@@ -0,0 +1,39 @@
+#include "Objects.h"
+#include <iostream>
+using namespace std;
+
+Objects()
+{
+
+}
+
+string Objects::moveObjects(string loc)
+{
+    
+
+}
+
+string Objects::getLocation()
+{
+    return location;
+}
+
+string Objects::getDonor()
+{
+    return donor;
+}
+
+string Objects::getDateRetreived()
+{
+    return dateRetreived;
+}
+
+int Objects::getWeight()
+{
+    return weight;
+}
+
+int Objects::getSerialNumber()
+{
+    return serialNum;
+}
