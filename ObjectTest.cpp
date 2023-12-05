b1edbcc801397c67208a421197e4f73ea1f5ed49
Gabe Greenberg <gabe.greenberg@richmond.edu>
2023-12-05 11:14:58 -0500

    ObjectTest

---
+++ ObjectTest.cpp
@@ -0,0 +1,76 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <crow.h>
+#include <doctest.h>
+#include <string>
+#include "Object.h"
+
+using namespace std;  
+using namespace crow;  
+
+
+TEST_CASE("Test Object Class")
+{
+
+    //Testing Object constructor
+    SUBCASE("Testing the Object constructor")
+    {
+        //create new Object class loaded from json
+        Object testObject(json::load(R"({"dateRetrieved":"N/A","weight":"32 Lbs","donor":"n/A","serialNum":"1","location":"Unsorted","name":"Jane Doe"})"));
+
+        //Check that the constructor properly loaded the value
+        CHECK(testObject.getDateRetrieved() == "N/A");
+        CHECK(testObject.getWeight() == "32 Lbs");
+        CHECK(testObject.getDonor() == "n/A");
+        CHECK(testObject.getSerialNum() == "1");
+        CHECK(testObject.getLocation() == "Unsorted");
+        CHECK(testObject.getName() == "Jane Doe");
+
+    }
+
+    //Testing convertToJson method
+    SUBCASE("Testing the convertToJson method")
+    {
+        //create new Object class loaded from json
+        Object testObject(json::load(R"({"dateRetrieved":"N/A","weight":"32 Lbs","donor":"n/A","serialNum":"1","location":"Unsorted","name":"Jane Doe"})"));
+
+        // Convert the Object class to json using the convertToJson method.
+        json::wvalue jsonOutput = testObject.convertToJson();
+
+        // Convert back to a json read value for testing.
+        json::rvalue jsonReadValue = json::load(jsonOutput.dump());
+
+        //Check the values
+        CHECK(jsonReadValue["dateRetrieved"].s() == "N/A");
+        CHECK(jsonReadValue["weight"].s() == "32 Lbs");
+        CHECK(jsonReadValue["donor"].s() == "n/A");
+        CHECK(jsonReadValue["serialNum"].s() == "1");
+        CHECK(jsonReadValue["location"].s() == "Unsorted");
+        CHECK(jsonReadValue["name"].s() == "Jane Doe");
+    }
+    //Testing updateFromJson method
+    SUBCASE("Testing updateFromJson method")
+    {
+        //create new Object class loaded from json
+        Object testObject(json::load(R"({"dateRetrieved":"N/A","weight":"32 Lbs","donor":"n/A","serialNum":"1","location":"Unsorted","name":"Jane Doe"})"));
+
+        //Create the updated json
+        json::rvalue updateJson = json::load(R"({"name":"DummyObject","location":"Unsorted","serialNum":"2","donor":"n/A","weight":"32 Lbs","dateRetrieved":"N/A"})");
+
+        //Update the Object using the updateFromJson method
+        testObject.updateFromJson(updateJson);
+
+        //Check the updated values
+        CHECK(testObject.getName() == "DummyObject");
+        CHECK(testObject.getLocation() == "Unsorted");
+        CHECK(testObject.getSerialNum() == "2");
+        CHECK(testObject.getDonor() == "n/A");
+        CHECK(testObject.getWeight() == "32 Lbs");
+        CHECK(testObject.getDateRetrieved() == "N/A");
+
+
+
+    }
+
+
+
+}
